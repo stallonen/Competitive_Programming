06f9af1753e3ae1e3a25ce195314bc1f5d74864f
stallonen <neelroboinfo365@gmail.com>
2016-03-30 11:13:28 +0530

    Edited CPTTRN2

--- Spoj/Basics/CPTTRN2.cpp
+++ Spoj/Basics/CPTTRN2.cpp
@@ -3,31 +3,32 @@
 using namespace std;
 
 int main() {
-
-	int t;
-	cin >> t;
-
-	while (t--) {
-		int l,c;
-		cin >> l >> c;
-
-		for(int i = 1; i <= l ; i++){
-			for(int j = 1; j <= c ; j++){
-
-				if (i == 1 || i == l) {
-				  cout << "*";
-				}
-				else if (j == 1 || j == c) {
-					cout << "*";
-				}
-				else {
-					cout << ".";
-				}
-			}
-			cout << endl;
-		}
-		cout << endl;
-	}
-
-	return 0;
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int l,c;
+        cin >> l >> c;
+        
+        for(int i = 1; i <= l ; i++){
+            for(int j = 1; j <= c ; j++){
+                
+                if (i == 1 || i == l) {
+                    cout << "*";
+                }
+                else if (j == 1 || j == c) {
+                    cout << "*";
+                }
+                else {
+                    cout << ".";
+                }
+            }		
+            cout << endl;
+        }
+        cout << endl;
+    }
+    
+    return 0;
 }
+
